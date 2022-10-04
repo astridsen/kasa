610f3d76759ef55a0048acba4c8fd8c5d7e5f3f9
astridsen <103685822+astridsen@users.noreply.github.com>
2022-10-04 11:09:55 +0200

    Slider dev

--- src/components/Layout/slider.jsx
+++ src/components/Layout/slider.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 //Style
 import arrowLeft from "../../assets/arrow-left.svg";
 import arrowRight from "../../assets/arrow-right.svg";
+import styles from "../../styles/slider.module.css"
 
 const Slider = ({ slides }) => {
   const [current, setCurrent] = useState(0);
@@ -18,25 +19,23 @@ const Slider = ({ slides }) => {
   };
 
   return (
-    <div>
+    <div className={styles.sliderWrapper}>
       {slides.map((picture, index) => {
         return (
-          <div
-            key={index}
-          >
+          <div key={index}>
             {index === current && (
-              <img src={picture} alt="" />
+              <img src={picture} alt="" className={styles.sliderPicture} />
             )}
           </div>
         );
       })}
       {length > 1 ? (
         <>
-          <div onClick={prevSlide}>
-            <img src={arrowLeft} alt="" />
+          <div className={styles.arrowLeft} onClick={prevSlide}>
+            <img src={arrowLeft} alt="" className={styles.arrowLeftIcon} />
           </div>
-          <div onClick={nextSlide}>
-            <img src={arrowRight} alt="" />
+          <div className={styles.arrowRight} onClick={nextSlide}>
+            <img src={arrowRight} alt="" className={styles.arrowRightIcon} />
           </div>
         </>
       ) : null}
