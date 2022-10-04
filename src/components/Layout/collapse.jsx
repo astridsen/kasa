517373440475875c1e0900ec689c9fb4b3d2e3ef
astridsen <103685822+astridsen@users.noreply.github.com>
2022-10-04 10:18:16 +0200

    AboutPage finish

--- src/components/Layout/collapse.jsx
+++ src/components/Layout/collapse.jsx
@@ -1,42 +1,30 @@
 import React from "react";
+import { useState } from "react";
+import ArrowUp from '../../assets/arrow-up.png';
+import ArrowDown from '../../assets/arrow-down.png';
 import styles from '../../styles/collapse.module.css';
 
 const Collapse = ({title, content}) => {
-    const isCollapse = (e) => {
-        e.preventDefault();
-        const divText = e.target.nextSibling;
-        const arrow = e.target.lastChild;
-    
-        if (!divText.classList.contains("show")) {
-          divText.classList.add("show");
-          arrow.classList.add("rotate");
-        } else {
-          divText.classList.remove("show");
-          arrow.classList.remove("rotate");
-        }
-      };
-      return (
-        <div className={styles.collapse}>
-          <button type="button" className={styles.collapseButton} onClick={isCollapse}>
-            {title}
-    
-            <p className={styles.collapseArrow}>&lt;</p>
-          </button>
-          <div className={styles.collapseContent}>
-            {Array.isArray(content) ? (
-              <ul>
-                {content.map((equipment, index) => (
-                  <li key={index}>
-                    {equipment}
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p className={styles.collapseText}>{content}</p>
-            )}
-          </div>
-        </div>
-      );
+    const [open, setOpen] = useState(false);
+
+  return (
+    <div className={styles.collapseItem}>
+      <div 
+        className={styles.collapseTitle } onClick={() => setOpen(!open)}
+      >
+        { title }
+        { open ? <img src={ArrowUp} alt="arrow-up"/> : <img src={ArrowDown} alt="arrow-down" /> }
+      </div>
+      <div
+        className={styles.collapseContent}
+        style={{ 
+          display: `${open ? "block" : "none"}`, 
+        }}
+      >
+        {content}
+      </div>
+    </div>
+  );
 }
 
 export default Collapse;
--- src/pages/aboutPage.jsx
+++ src/pages/aboutPage.jsx
@@ -3,15 +3,17 @@ import Banner from "../components/Layout/banner";
 import Collapse from "../components/Layout/collapse";
 import imageBanner from "../assets/aboutBackground.svg";
 
+import styles from '../styles/collapse.module.css'
+
 const AboutPage = () => {
     return (
         <div>
             <Banner image={imageBanner} />
-            <div className="about__dropdowns">
-                <Collapse title="fiabilité" content={'Les annonces postées sur Kasa'} />
-                <Collapse title="respect" content={'La bienveillance fait partie'} />
-                <Collapse title="service" content={'Nos équipes se tiennent'} />
-                <Collapse title="sécurité" content={'La sécurité est la priorité'} />
+            <div className={styles.collapseWrapper}>
+                <Collapse title="Fiabilité" content={"Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."} />
+                <Collapse title="Respect" content={"La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."} />
+                <Collapse title="Service" content={"Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."} />
+                <Collapse title="Sécurité" content={"La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."} />
             </div>
         </div>
     )
